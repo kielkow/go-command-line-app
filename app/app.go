f79c51edcf8a5839f03397277437a63d5a779cda
kielkow <matheuskiel@fiorifer.com.br>
2020-11-24 11:06:22 -0300

    Search by servers name

--- app/app.go
+++ app/app.go
@@ -15,17 +15,25 @@ func Generate() *cli.App {
 	app.Name = "CommandLineApp"
 	app.Usage = "Search by server names and IPs"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com"
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search by IPs address on internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com"
-				}
-			},
+			Name:   "ip",
+			Usage:  "Search by IPs address on internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search by servers names on internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -44,5 +52,20 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+// Example of command -> go run main.go servers --host google.com
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
